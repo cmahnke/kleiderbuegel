232590419739383b5e4f621815c72db834070804
Christian Mahnke <cmahnke@gmail.com>
2021-05-15 17:47:55 +0200

    Draft of SVG preview renderer

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,9 +2,14 @@
 
 convert "Source Files/Logo/Logo.psd[0]" -flatten -layers merge static/images/kleiderbuegel.png
 
+# Generate Previews
+python3 scripts/preview.py
+#inkscape preview.svg --export-filename=preview.png
+#; mv {}.png $(dirname {})/$(basename {} .svg).png
+find content -name preview.svg -print -exec inkscape {} --export-filename=$(dirname {})/$(basename {} -s .svg).png  \;
+
 # Favicons
 SOURCE="static/images/kleiderbuegel.png" OPTIONS="-resize 128x128 -transparent white" ./themes/projektemacher-base/scripts/favicon.sh
-
 
 yarn install
 yarn svgo
