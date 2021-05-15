58ca60fd401c6484bab495573287efe2d806d85e
Christian Mahnke <cmahnke@gmail.com>
2021-05-15 19:31:17 +0200

    Added open graph previes for posts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,8 +5,7 @@ convert "Source Files/Logo/Logo.psd[0]" -flatten -layers merge static/images/kle
 # Generate Previews
 python3 scripts/preview.py
 #inkscape preview.svg --export-filename=preview.png
-#; mv {}.png $(dirname {})/$(basename {} .svg).png
-find content -name preview.svg -print -exec inkscape {} --export-filename=$(dirname {})/$(basename {} -s .svg).png  \;
+find content -name preview.svg -print -exec bash -c 'inkscape "{}" --export-filename=$(dirname "{}")/$(basename -s .svg "{}").png' \;
 
 # Favicons
 SOURCE="static/images/kleiderbuegel.png" OPTIONS="-resize 128x128 -transparent white" ./themes/projektemacher-base/scripts/favicon.sh
