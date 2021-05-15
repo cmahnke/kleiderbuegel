325fbe1b2e9749ec8358f3ff55a5a9b55bac59c9
Christian Mahnke <cmahnke@gmail.com>
2021-05-15 19:42:59 +0200

    moved generator script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 convert "Source Files/Logo/Logo.psd[0]" -flatten -layers merge static/images/kleiderbuegel.png
 
 # Generate Previews
-python3 scripts/preview.py
+python3 ./themes/projektemacher-base/scripts/preview.py
 #inkscape preview.svg --export-filename=preview.png
 find content -name preview.svg -print -exec bash -c 'inkscape "{}" --export-filename=$(dirname "{}")/$(basename -s .svg "{}").png' \;
 
