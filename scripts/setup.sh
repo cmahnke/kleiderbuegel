7415b873bb0e9f4d5db9e127bd69646ee918ef2b
Christian Mahnke <cmahnke@gmail.com>
2021-05-16 15:35:39 +0200

    Updated base theme

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,9 +3,7 @@
 convert "Source Files/Logo/Logo.psd[0]" -flatten -layers merge static/images/kleiderbuegel.png
 
 # Generate Previews
-python3 ./themes/projektemacher-base/scripts/preview.py
-#inkscape preview.svg --export-filename=preview.png
-find content -name preview.svg -print -exec bash -c 'inkscape "{}" --export-filename=$(dirname "{}")/$(basename -s .svg "{}").png' \;
+./themes/projektemacher-base/scripts/preview.sh
 
 # Favicons
 SOURCE="static/images/kleiderbuegel.png" OPTIONS="-resize 128x128 -transparent white" ./themes/projektemacher-base/scripts/favicon.sh
