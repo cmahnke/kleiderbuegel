6a2db4e72232e3106d039d5e362e088d5b5259fd
Christian Mahnke <cmahnke@gmail.com>
2022-10-11 09:47:14 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,18 @@ convert "Source Files/Logo/Logo.psd[0]" -flatten -layers merge static/images/kle
 # Generate Previews
 ./themes/projektemacher-base/scripts/preview.sh
 
+#NPM dependencies
+echo "Calling theme scripts"
+for SCRIPT in $PWD/themes/projektemacher-base/scripts/init/*.sh ; do
+    echo "Running $SCRIPT"
+    bash "$SCRIPT"
+    ERR=$?
+    if [ $ERR -ne 0 ] ; then
+        echo "Execution of '$SCRIPT' failed!"
+        exit $ERR
+    fi
+done
+
 # Favicons
 SOURCE="static/images/kleiderbuegel.png" OPTIONS="-resize 128x128 -transparent white" ./themes/projektemacher-base/scripts/favicon.sh
 
