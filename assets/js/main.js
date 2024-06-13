fb5cd329f6f4eee91d1359626cda28d9fc5a73f7
Christian Mahnke <cmahnke@gmail.com>
2024-06-13 13:32:44 +0200

    Minor layout fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,12 +9,28 @@ window.$ = window.jQuery = require('jquery');
 require('jquery.flipster');
 
 window.initFlipster = function(elem) {
+  console.log(elem);
     var buegel = $(elem).flipster({
         spacing: -0.7
     });
     $(elem).find('.buegel').each(function() {
         $(this).on('click', function() {
             var id = $(this).data('content-id');
+            var subList = $('#' + id + ' ul');
+            subList.removeClass('flipster__container');
+            subList.removeAttr('style');
+            subList.find(".flipster__item__content, .flipster__item, .flipster__item--future").each(function() {
+              $(this).removeClass('flipster__item__content');
+              $(this).removeClass('flipster__item');
+              $(this).removeClass('flipster__item--future');
+              // TODO: Find a better way
+              $(this).removeClass('flipster__item--future-1');
+              $(this).removeClass('flipster__item--future-2');
+              $(this).removeClass('flipster__item--future-3');
+              $(this).removeClass('flipster__item--future-4');
+              $(this).removeAttr('style');
+            });
+
             var content = $('#' + id).html();
             $('#kleidungsstueck').html(content);
             if (!$('#kleidungsstueck').hasClass('active')) {
