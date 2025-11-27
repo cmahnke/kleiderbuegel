fdb6bf8f328a9b4b5ecbc628e10c23c8b4696277
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2025-11-27 03:36:52 +0100

    update print map

--- assets/js/maps/leaflet-map.js
+++ assets/js/maps/leaflet-map.js
@@ -1,148 +1,165 @@
 import L from 'leaflet';
 import 'leaflet.markercluster';
 import 'leaflet.vectorgrid';
-import 'mapbox-gl-leaflet';
+import '@maplibre/maplibre-gl-leaflet';
 
 import * as style from './kleiderbügel-style.json';
 
-var layers = {};
+var layers = {"shortbread": style};
 
-layers['wiki'] = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png?lang=en', {
-    attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-});
+// TODO: This wil certainly fail with any other OpenLayers marker structure
+function leafletIcon(olIconStyle) {
+  const src = olIconStyle.src;
+  const scale = olIconStyle.scale || 1;
+  const olAnchor = olIconStyle.anchor || [0.5, 0.5];
 
-layers['osm'] = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-});
-
-var vectorStyles = {};
-
-vectorStyles['shortbread'] = L.mapboxGL({
-    // The style can be a URL to a JSON file or a style object
-    style: style
-})
-
-export function initMap(element, url, source, cluster, marker, style, bbox) {
-
-  // Languages
-  var lang = 'en';
-  if (document.documentElement.lang !== undefined) {
-      /* TODO: Check for lang locale combinations here: "de-de" instead of "de" will currently break this. */
-      lang = document.documentElement.lang;
+  if (!src) {
+    return Promise.resolve(null);
   }
-  var toolTips = { 'de': {'zoomIn': 'Vergrößern', 'zoomOut': 'Verkleinern', 'fullscreen': 'Vollbildansicht', 'rotate': 'Rotation zurücksetzen', 'rotateLeft': '90° nach links drehen', 'rotateRight': '90° nach rechst drehen'},
-                   'en': {'zoomIn': 'Zoom in', 'zoomOut': 'Zoom out', 'fullscreen': 'Toggle full-screen'}};
 
-    // Base layer
-    var baseLayer;
-    if (style !== undefined && style in vectorStyles) {
-        baseLayer = vectorStyles[style];
-    } else if (source !== undefined && source !== '') {
-        if (source in layers) {
-            baseLayer = layers[source];
-        } else {
-            baseLayer = L.tileLayer(source);
-        }
-    } else {
-      baseLayer = layers['osm'];
+  return new Promise((resolve) => {
+    const img = new Image();
+    
+    img.onload = () => {
+      const naturalWidth = img.naturalWidth;
+      const naturalHeight = img.naturalHeight;
+
+      const finalWidth = naturalWidth * scale;
+      const finalHeight = naturalHeight * scale;
+
+      let anchorX = olAnchor[0];
+      let anchorY = olAnchor[1];
+
+      if (olIconStyle.anchorXUnits === 'fraction' || anchorX <= 1) {
+        anchorX = finalWidth * anchorX;
+      }
+      if (olIconStyle.anchorYUnits === 'fraction' || anchorY <= 1) {
+        anchorY = finalHeight * anchorY;
+      }
+      
+      const leafletIcon = L.icon({
+        iconUrl: src,
+        iconSize: [finalWidth, finalHeight],
+        iconAnchor: [anchorX, anchorY],
+        popupAnchor: [anchorX, anchorY] 
+      });
+      
+      resolve(leafletIcon);
+    };
+
+    img.onerror = () => {
+      resolve(null);
+    };
+
+    img.src = src;
+  });
+}
+
+export async function initMap(element, geojson, source, cluster, marker, style, bbox) {
+    console.log(bbox);
+    if (!style in layers) {
+        throw new Error(`'${style}' is not a valid style.`);
     }
 
+    let markerOptions = {};
+
+    if (marker) {
+        //TODO: Check if we need to be sync her to be not outrunned by the layout process.
+        let leafletMarker = await leafletIcon(marker)
+        
+        markerOptions.icon = leafletMarker
+    }
+
+    let geoJsonLayer, center;
+    if (geojson !== undefined) {
+        geojson = JSON.parse(geojson);
+        geoJsonLayer = L.geoJson(geojson, {
+                            pointToLayer: function (feature, latlng) {
+                                return L.marker(latlng, markerOptions);
+                            },
+                            onEachFeature: function (feature, layer) {
+                                if (feature.properties && (feature.properties.name || feature.properties.popupContent)) {
+                                    let popupContent = '';
+                                    if (feature.properties.name) {
+                                        popupContent += '<h1>' + feature.properties.name + '</h1>';
+                                    }
+                                    if (feature.properties.popupContent) {
+                                        popupContent += feature.properties.popupContent;
+                                    }
+                                    layer.bindPopup(popupContent);
+                                }
+                            }
+                        });
+        center = geoJsonLayer.getBounds().getCenter();
+    }
+    
     var map = L.map(element, {
-        layers: [baseLayer],
-        zoomControl: false, // We add it with custom tooltips later
-        renderer: L.svg() // Use SVG renderer as requested
-    });
+        maxBounds: [[180, -Infinity], [-180, Infinity]],
+        maxBoundsViscosity: 1,
+        zoomControl: false,
+        renderer: L.svg(),
+        minZoom: 1,
+        maxZoom: 15,
+        center: center,
+        zoom: 5,
+        zoomSnap: .1
+    });    
 
-    L.control.zoom({
-        zoomInTitle: toolTips[lang]['zoomIn'],
-        zoomOutTitle: toolTips[lang]['zoomOut']
+    if (bbox && bbox.length === 4) {
+        const bounds = [[bbox[1], bbox[0]], [bbox[3], bbox[2]]];
+        map.fitBounds(bounds, { padding: [30, 30] });
+    }
+    
+    var gl = L.maplibreGL({
+        style: layers[style]
     }).addTo(map);
+    
 
-    fetch(url)
-        .then(function(response) {
-            response
-                .json()
-                .then(function(geojson) {
-                    let geoJsonLayer;
-                    let markerOptions = {};
+    if (cluster !== undefined && cluster) {
+        let markers = L.markerClusterGroup({
+            //showCoverageOnHover: false,
+            maxClusterRadius: 40,
+            iconCreateFunction: function(cluster) {
+                const count = cluster.getChildCount();
+                const icon = markerOptions.icon;
 
-                    if (marker) {
-                        if (typeof marker !== 'object') {
-                            try {
-                                marker = JSON.parse(marker);
-                            } catch (e) {
-                                console.warn(`Can't parse marker ${marker}`);
-                            }
-                        }
-                        markerOptions.icon = L.icon(marker);
-                    }
+                if (!icon) {
+                    return L.divIcon({ html: '<b>' + count + '</b>', className: 'marker-cluster' });
+                }
 
-                    if (cluster !== undefined && cluster) {
-                        geoJsonLayer = L.markerClusterGroup({
-                            // Other cluster options can go here
-                        });
-                        const markers = L.geoJSON(geojson, {
-                            pointToLayer: function (feature, latlng) {
-                                return L.marker(latlng, markerOptions);
-                            },
-                            onEachFeature: function (feature, layer) {
-                                if (feature.properties && (feature.properties.name || feature.properties.popupContent)) {
-                                    let popupContent = '';
-                                    if (feature.properties.name) {
-                                        popupContent += '<h1>' + feature.properties.name + '</h1>';
-                                    }
-                                    if (feature.properties.popupContent) {
-                                        popupContent += feature.properties.popupContent;
-                                    }
-                                    layer.bindPopup(popupContent);
-                                }
-                            }
-                        });
-                        geoJsonLayer.addLayer(markers);
-                    } else {
-                        geoJsonLayer = L.geoJSON(geojson, {
-                            pointToLayer: function (feature, latlng) {
-                                return L.marker(latlng, markerOptions);
-                            },
-                            onEachFeature: function (feature, layer) {
-                                if (feature.properties && (feature.properties.name || feature.properties.popupContent)) {
-                                    let popupContent = '';
-                                    if (feature.properties.name) {
-                                        popupContent += '<h1>' + feature.properties.name + '</h1>';
-                                    }
-                                    if (feature.properties.popupContent) {
-                                        popupContent += feature.properties.popupContent;
-                                    }
-                                    layer.bindPopup(popupContent);
-                                }
-                            }
-                        });
-                    }
+                const iconUrl = icon.options.iconUrl;
+                const iconSize = icon.options.iconSize;
 
-                    map.addLayer(geoJsonLayer);
-
-                    if (bbox && bbox.length === 4) {
-                        // bbox is [minLon, minLat, maxLon, maxLat]
-                        // Leaflet bounds are [[minLat, minLon], [maxLat, maxLon]]
-                        const bounds = [[bbox[1], bbox[0]], [bbox[3], bbox[2]]];
-                        map.fitBounds(bounds, { padding: [30, 30] });
-                    } else {
-                        map.fitBounds(geoJsonLayer.getBounds(), { padding: [30, 30] });
-                    }
-                })
-                .catch(function(body) {
-                    console.log('Could not read GeoJSON. ' + body);
+                return L.divIcon({
+                    html: `<img src="${iconUrl}" style="width:${iconSize[0]}px;height:${iconSize[1]}px;"><span class="cluster-count">${count}</span>`,
+                    className: 'custom-marker-cluster',
+                    iconSize: iconSize
                 });
-        })
-        .catch(function() {
-            console.log('Could not read data from URL.');
+            }
         });
+        markers.addLayer(geoJsonLayer);
+        map.addLayer(markers);
+    } else {
+        map.addLayer(geoJsonLayer);
+    }
 
-    // Remove popup elements if they exist, as Leaflet handles its own popups
+    if (bbox && bbox.length === 4) {
+        // bbox is [minLon, minLat, maxLon, maxLat]
+        // Leaflet bounds are [[minLat, minLon], [maxLat, maxLon]]
+        const bounds = [[bbox[1], bbox[0]], [bbox[3], bbox[2]]];
+        map.fitBounds(bounds, { padding: [30, 30] });
+    } else {
+        map.fitBounds(geoJsonLayer.getBounds(), { padding: [30, 30] });
+    }
+    // TODO remove this from partial
     const popupContainer = document.getElementById(element + '-popup');
     if (popupContainer) {
         popupContainer.remove();
     }
+
+    setTimeout(function(){
+        map.invalidateSize(true);}
+    ,1000)
 
     return map;
 }
---
+++ vivliostyle.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Kleiderbügel', 
+  author: 'Christian Mahnke',
+  language: 'de',
+  entry: ['docs/print.html'],
+  static: {
+    '/__vivliostyle-viewer/js/': 'docs/js/',
+    '/__vivliostyle-viewer/images/': 'docs/images/'
+  },
+};
